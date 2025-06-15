c4d3f743777d25d6a8f5abfcab3460a1f24ba28a
theskywalker94 <lucarusso2494@gmail.com>
2025-06-15 20:39:06 +0200

    Update modello_relazionale.sql
    Added constraints.

--- modello_relazionale.sql
+++ modello_relazionale.sql
@@ -2,42 +2,42 @@
 
 CREATE TABLE Aeroporto (
   id_aeroporto SERIAL PRIMARY KEY,
-  nome VARCHAR(100),
-  citta VARCHAR(100),
-  paese VARCHAR(100),
-  codice_IATA CHAR(3) UNIQUE
+  nome VARCHAR(100) NOT NULL,
+  citta VARCHAR(100) NOT NULL,
+  paese VARCHAR(100) NOT NULL,
+  codice_IATA CHAR(3) UNIQUE NOT NULL
 );
 
 CREATE TABLE Tratta (
   id_tratta SERIAL PRIMARY KEY,
   aeroporto_partenza INT REFERENCES Aeroporto(id_aeroporto),
   aeroporto_arrivo INT REFERENCES Aeroporto(id_aeroporto),
-  durata_minuti INT -- correggere tipo
+  durata_minuti INT NOT NULL -- correggere tipo
 );
 
 CREATE TABLE Volo (
   id_volo SERIAL PRIMARY KEY,
   id_tratta INT REFERENCES Tratta(id_tratta),
-  data_partenza DATE,
-  ora_partenza TIME,
-  numero_volo VARCHAR(10),
-  stato VARCHAR(50)
+  data_partenza DATE NOT NULL,
+  ora_partenza TIME NOT NULL,
+  numero_volo VARCHAR(10) NOT NULL,
+  stato VARCHAR(50) CHECK (stato IN ('programmato', 'cancellato', 'completato'))
 );
 
 CREATE TABLE Passeggero (
   id_passeggero SERIAL PRIMARY KEY,
-  nome VARCHAR(100),
-  cognome VARCHAR(100),
-  email VARCHAR(150),
+  nome VARCHAR(100) NOT NULL,
+  cognome VARCHAR(100) NOT NULL,
+  email VARCHAR(150) NOT NULL,
   telefono VARCHAR(20),
-  documento_identita VARCHAR(50)
+  documento_identita VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Prenotazione (
   id_prenotazione SERIAL PRIMARY KEY,
-  data_prenotazione DATE,
-  metodo_pagamento VARCHAR(50),
-  totale_pagato DECIMAL(10,2),
+  data_prenotazione DATE NOT NULL,
+  metodo_pagamento VARCHAR(50) NOT NULL,
+  totale_pagato DECIMAL(10,2) NOT NULL,
   id_passeggero INT REFERENCES Passeggero(id_passeggero)
 );
 
@@ -45,9 +45,9 @@ CREATE TABLE Biglietto (
   id_biglietto SERIAL PRIMARY KEY,
   id_prenotazione INT REFERENCES Prenotazione(id_prenotazione),
   id_volo INT REFERENCES Volo(id_volo),
-  classe VARCHAR(20),
-  posto VARCHAR(5),
-  check_in_effettuato BOOLEAN,
-  qr_code TEXT, -- oppure VARCHAR?
-  stato VARCHAR(20)
+  classe VARCHAR(20) CHECK (classe IN ('economy', 'business')),
+  posto VARCHAR(5) NOT NULL,
+  check_in_effettuato BOOLEAN DEFAULT FALSE,
+  qr_code TEXT UNIQUE, -- oppure VARCHAR?
+  stato VARCHAR(20) CHECK (stato IN ('valido', 'annullato', 'utilizzato'))
 );
