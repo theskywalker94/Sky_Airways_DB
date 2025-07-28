ec0e7c457e9565078c7306d1afa6604a3c2d623e
Luca Russo <lucarusso2494@gmail.com>
2025-07-28 21:30:52 +0200

    Update modello_relazionale.sql
    Big changes to first 4 tables, simplified them all

--- modello_relazionale.sql
+++ modello_relazionale.sql
@@ -1,50 +1,49 @@
 -- modello_relazionale.sql
 
 CREATE TABLE Aeroporto (
-  id_aeroporto SERIAL PRIMARY KEY,
+  id_aeroporto CHAR(3) PRIMARY KEY,
   nome VARCHAR(100) NOT NULL,
   citta VARCHAR(100) NOT NULL,
-  paese VARCHAR(100) NOT NULL,
-  codice_IATA CHAR(3) UNIQUE NOT NULL
+  paese VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE Tratta (
   id_tratta SERIAL PRIMARY KEY,
-  aeroporto_partenza INT REFERENCES Aeroporto(id_aeroporto),
-  aeroporto_arrivo INT REFERENCES Aeroporto(id_aeroporto),
-  durata_prevista TIME NOT NULL
+  aeroporto_partenza CHAR(3) REFERENCES Aeroporto(id_aeroporto),
+  aeroporto_arrivo CHAR(3) REFERENCES Aeroporto(id_aeroporto),
 );
 
 CREATE TABLE Volo (
-  id_volo SERIAL PRIMARY KEY,
-  id_tratta INT REFERENCES Tratta(id_tratta),
+  id_volo VARCHAR(6) PRIMARY KEY,
+  aeroporto_partenza CHAR(3) REFERENCES Aeroporto(id_aeroporto),
+  aeroporto_arrivo CHAR(3) REFERENCES Aeroporto(id_aeroporto),
   data_partenza DATE NOT NULL,
   ora_partenza TIME NOT NULL,
-  numero_volo VARCHAR(10) NOT NULL,
+  data_arrivo DATE NOT NULL,
+  ora_arrivo TIME NOT NULL,
   stato VARCHAR(50) CHECK (stato IN ('programmato', 'cancellato', 'completato')),
   modello VARCHAR(10) CHECK (modello IN ('B787', 'A320neo'))
 );
 
 CREATE TABLE Passeggero (
-  id_passeggero SERIAL PRIMARY KEY,
+  id_passeggero VARCHAR(6) PRIMARY KEY,
   nome VARCHAR(100) NOT NULL,
   cognome VARCHAR(100) NOT NULL,
   email VARCHAR(150) NOT NULL,
-  telefono VARCHAR(20),
-  documento_identita VARCHAR(50) NOT NULL
+  telefono VARCHAR(50)
 );
 
 CREATE TABLE Prenotazione (
   id_prenotazione SERIAL PRIMARY KEY,
   data_prenotazione DATE NOT NULL,
   metodo_pagamento VARCHAR(50) NOT NULL,
-  id_passeggero INT REFERENCES Passeggero(id_passeggero)
+  id_passeggero VARCHAR(6) REFERENCES Passeggero(id_passeggero)
 );
 
 CREATE TABLE Biglietto (
   id_biglietto SERIAL PRIMARY KEY,
   id_prenotazione INT REFERENCES Prenotazione(id_prenotazione),
-  id_volo INT REFERENCES Volo(id_volo),
+  id_volo VARCHAR(6) REFERENCES Volo(id_volo),
   classe VARCHAR(20) CHECK (classe IN ('economy', 'business')),
   posto VARCHAR(5) NOT NULL,
   check_in_effettuato BOOLEAN DEFAULT FALSE,
